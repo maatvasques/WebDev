aec59df31320b1a5accb3ec56fafe97271886cf2
maatvasques <matheusvasquessantos@gmail.com>
2025-04-07 22:37:30 -0300

    Add files via upload

---
+++ WebDev_Aula5/app.js
@@ -0,0 +1,21 @@
+// alert("Hello World")
+// prompt("Qual é o seu nome")
+// let nome = prompt("Qual é o seu nome?")
+// let idade = prompt("Qual a sua idade?")
+// let profissao = prompt("Qual a sua profissão?")
+
+// alert(`Ola ${nome}, tudo bem? Você tem ${idade} anos e trabalha como ${profissao}`)
+
+//existem as variaveis var, let, const(const nn muda)
+// let nome = "MATHEUS"
+// let idade = 70
+//var só devem conter letras/numeros/$ e _, nn pode comecar com numero, nn é recomendado usar acento
+
+let numero1 = Number(prompt("Fale um numero"))
+let numero2 = Number(prompt("Fale outro numero"))
+
+alert(numero1 + numero2)
+
+
+
+console.log(typeof idade)
