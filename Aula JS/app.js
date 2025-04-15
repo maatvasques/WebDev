27ffdee2a589edca1b7f8361bdd86f92e1b404a4
maatvasques <matheusvasquessantos@gmail.com>
2025-04-14 22:34:38 -0300

    Add files via upload

---
+++ Aula JS/app.js
@@ -0,0 +1,40 @@
+//{
+// let nome = "Caio";
+// if(nome == "Caio") {
+//     console.log("É você mesmo Caio")
+// }
+// else if (nome === "Fulano") {
+//     console.log("Quem é você?")
+// }
+// else {	
+//     console.log("Não é você Caio")
+// }
+// let nome = "Matheus";
+// (condição) ? (true) : (false)
+// nome === "Matheus" ? console.log("É você mesmo Matheus") : console.log("Não é você Matheus")
+// nome === "Matheus" ? console.log("É você mesmo Matheus") : console.log("Não é você Matheus")
+// }
+
+// && significa (E), || significa (OU), ! significa (NÃO).
+{/* <a>
+    {/* let usuario = prompt("Digite o primeiro usuario: ");
+    let senha = prompt("Digite sua primeira senha:")
+    
+    let usuario2 = prompt("Digite o segundo usuario: ");
+    let senha2 = prompt("Digite sua segunda senha:")
+    
+    let usuario3 = prompt("Digite o terceira usuario: ");
+    let senha3 = prompt("Digite sua terceira senha:")
+    
+    if ((usuario === "1234" && senha === "1234") ||
+        (usuario2 === "567" && senha2 === "567") ||
+        (usuario3 === "890" && senha3 === "890")) {
+        alert("Login efetuado com sucesso")
+    
+    } else {
+        alert("Login ou senha incorretos")
+    } */}
+
+
+
+
