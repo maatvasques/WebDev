69107e63a10342cecfa8b3a750aa10a5ce172aa8
maatvasques <matheusvasquessantos@gmail.com>
2025-04-14 21:47:09 -0300

    Update app.js

--- WebDev_Aula5/app.js
+++ WebDev_Aula5/app.js
@@ -1,16 +1,21 @@
+// alert é um pop up normal, prompt, e confirm(ok e cancel)
 // alert("Hello World")
 // prompt("Qual é o seu nome")
 // let nome = prompt("Qual é o seu nome?")
 // let idade = prompt("Qual a sua idade?")
 // let profissao = prompt("Qual a sua profissão?")
-
+// tem que ter craze(`) para conseguir concatenar variaveis em strings
 // alert(`Ola ${nome}, tudo bem? Você tem ${idade} anos e trabalha como ${profissao}`)
 
 //existem as variaveis var, let, const(const nn muda)
 // let nome = "MATHEUS"
 // let idade = 70
 //var só devem conter letras/numeros/$ e _, nn pode comecar com numero, nn é recomendado usar acento
+//da para guardar numeros, strings, booleanos, null e undefined, em numeros existe o infinito e NaN (not a number)
+//typeof (informacao) para saber a variavel do ngc 
 
+// o number serve para transformar o prompt que é string em numero
+//parseFloat para numeros decimais e parseInt para numeros inteiros tambem serve.
 let numero1 = Number(prompt("Fale um numero"))
 let numero2 = Number(prompt("Fale outro numero"))
 
